2631b054f43426ccac919a1f8e5bc249cf0a4d9f
Toon De True <toondetrue@Toons-Mac.local>
2016-12-20 10:30:25 +0100

    Initial Commit

---
+++ ShapeClickerTests/ShapeClickerTests.swift
@@ -0,0 +1,36 @@
+//
+//  ShapeClickerTests.swift
+//  ShapeClickerTests
+//
+//  Created by Toon De True on 20/12/2016.
+//  Copyright © 2016 Toon De True. All rights reserved.
+//
+
+import XCTest
+@testable import ShapeClicker
+
+class ShapeClickerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
